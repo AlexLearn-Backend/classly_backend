a4707a767224d109db8957512b93f4e9cccbe298
Alex Kimari <officialex236@gmail.com>
2026-06-11 13:42:47 +0300

    Added a comprehensive catch for unmatched denied requests

--- src/middlewares/security.ts
+++ src/middlewares/security.ts
@@ -86,6 +86,13 @@ export default async function securityMiddleware (
       })
     }
 
+    if (decision.isDenied()) {
+      return res.status(403).json({
+        error: "Forbidden!",
+        message: "Request denied by security policy."
+      })
+    }
+
     next()
 
   }
